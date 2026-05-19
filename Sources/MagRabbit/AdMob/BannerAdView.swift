75c15191ae3fd8194f138d493d259ab12885e147
snarfnet <82538502+snarfnet@users.noreply.github.com>
2026-05-19 14:04:49 +0900

    Add production AdMob banner

--- Sources/MagRabbit/AdMob/BannerAdView.swift
+++ Sources/MagRabbit/AdMob/BannerAdView.swift
@@ -1,9 +1,28 @@
 import SwiftUI
+import GoogleMobileAds
+import UIKit
 
 struct BannerAdView: View {
     var body: some View {
-        // AdMob banner placeholder - will be integrated later
-        Color.clear
+        BannerAdContainer(adSize: AdSizeBanner)
             .frame(height: 50)
     }
+}
+
+private struct BannerAdContainer: UIViewRepresentable {
+    let adSize: AdSize
+
+    func makeUIView(context: Context) -> BannerView {
+        let bannerView = BannerView(adSize: adSize)
+        bannerView.adUnitID = "ca-app-pub-9404799280370656/9988817066"
+        bannerView.rootViewController = UIApplication.shared.connectedScenes
+            .compactMap { $0 as? UIWindowScene }
+            .flatMap(\.windows)
+            .first { $0.isKeyWindow }?
+            .rootViewController
+        bannerView.load(Request())
+        return bannerView
+    }
+
+    func updateUIView(_ uiView: BannerView, context: Context) {}
 }
--- Sources/MagRabbit/MagRabbitApp.swift
+++ Sources/MagRabbit/MagRabbitApp.swift
@@ -1,7 +1,12 @@
 import SwiftUI
+import GoogleMobileAds
 
 @main
 struct MagRabbitApp: App {
+    init() {
+        MobileAds.shared.start { _ in }
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
